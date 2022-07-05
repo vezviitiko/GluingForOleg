5b271386e1d74ebdccb88ba602b45a3c4b521095
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2022-07-05 14:35:36 +0800

    fix

--- functions.py
+++ functions.py
@@ -36,29 +36,6 @@ def str_to_datetime(str):
 def datime_for_brdcfile(year,month,day,hour,minute,sec):
     return str(str(year)[2:]+'%02i' % int(month)+'%02i' % int(day)+' %02i' % int(hour)+':%02i' % int(minute)+':%02i' % int(sec))
 
-def str_to_num(str):
-    return float(str)
-
-def line_str_to_num(line,data_num_block):
-    print('line_str_to_num   ')
-    for str_nums in line.split():
-        if len(str_nums) > 20:
-            str_num = ''
-            i = 0
-            while i < len(str_nums):
-                num_zn = str_nums[18+i:].find('-')
-                if num_zn < 0:
-                    num_zn = str_nums.rfind('-')
-                    str_num = str_nums[num_zn:]
-                    break
-                str_num = str_nums[:18+i + num_zn]
-                data_num_block.append(str_to_num(str_num))
-                i+=18+num_zn
-            data_num_block.append(str_to_num(str_num))
-
-        else:
-            data_num_block.append(str_to_num(str_nums))
-
 def check_limit_sys(line, n_sys, num_line_start_block):
     match n_sys:
         case 1:

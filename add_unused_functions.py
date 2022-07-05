074888f1aa7b4a4f8bd31a1c511661a17ffb9309
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2022-07-05 14:36:01 +0800

    Create add_unused_functions.py

---
+++ add_unused_functions.py
@@ -0,0 +1,22 @@
+def str_to_num(str):
+    return float(str)
+
+def line_str_to_num(line,data_num_block):
+    print('line_str_to_num   ')
+    for str_nums in line.split():
+        if len(str_nums) > 20:
+            str_num = ''
+            i = 0
+            while i < len(str_nums):
+                num_zn = str_nums[18+i:].find('-')
+                if num_zn < 0:
+                    num_zn = str_nums.rfind('-')
+                    str_num = str_nums[num_zn:]
+                    break
+                str_num = str_nums[:18+i + num_zn]
+                data_num_block.append(str_to_num(str_num))
+                i+=18+num_zn
+            data_num_block.append(str_to_num(str_num))
+
+        else:
+            data_num_block.append(str_to_num(str_nums))

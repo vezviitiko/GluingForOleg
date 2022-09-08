ae6ed9ea5570757f3ae0bb5dd89df214f7e34530
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2022-09-08 09:54:07 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -52,7 +52,7 @@ def check_limit_sys(line, n_sys, num_line_start_block):
     elif n_sys == 3:
             if num_line_start_block == 4:
                 if line.find('      ') > 0:
-                    line = line.replace('                  ', ' 0.000000000000D+00')
+                    line = line.replace('                   ', ' 0.000000000000D+00')
                 elif len(line)<75:
                     line = line[:-1] +' 0.000000000000D+00\n'
                 return line
@@ -61,7 +61,7 @@ def check_limit_sys(line, n_sys, num_line_start_block):
     elif n_sys == 4:
             if num_line_start_block == 4:
                 if line.find('      ')>0:
-                    line = line.replace('                  ',' 0.000000000000D+00')
+                    line = line.replace('                   ', ' 0.000000000000D+00')
                 elif len(line)<75:
                     line = line[:-1] +' 0.000000000000D+00\n'
                 return line
